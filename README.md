bb07034bdf708b04bb9dcde86c1e7f8e56e16553
Jason <Jason@wildcards.world>
2022-03-25 13:14:12 +0200

    Basic template

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# token-recipient-splitter
+# Payment Splitter
+
+Send ERC20 tokens to multiple recipients equally.
+
+## Getting Started
+
+```
+git submodule update --init --recursive  ## initialize submodule dependencies
+yarn ## install development dependencies
+forge build
+forge test # add `-vvvvv` for detailed traces and debug information.
+```
---
+++ src/Contract.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity >=0.8.0;
+
+import "./test/utils/Console.sol";
+
+contract Contract {
+    mapping(address => bool) public isRecipient;
+    uint256 public numberOfRecipients;
+
+    modifier onlyRecipient() {
+        require(isRecipient[msg.sender], "not recipient");
+        _;
+    }
+
+    constructor(address[] memory initialRecipients) {
+        // TODO
+        console.log("Fill this function in");
+        console.log(initialRecipients);
+    }
+
+    function splitPay(uint256 amount) public {
+        // TODO
+        console.log("Fill this function in", amount);
+    }
+
+    function claimPayments() public onlyRecipient {
+        // TODO
+        console.log("Fill this function in");
+    }
+}
